3f93497941e52fdd72501c1b5dfdf20140924d91
Marcus Bergman <mraxus@gmail.com>
2016-02-07 14:17:00 +0100

    Make app work as a chat room

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var http = require('http');
-var WebSocketServer = require("ws").Server;
-var express = require("express");
+var WebSocketServer = require('ws').Server;
+var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 5000;
@@ -14,7 +14,8 @@ var Types = {
   INIT: 'INIT',
   TICK: 'TICK',
   ENTER: 'ENTER',
-  EXIT: 'EXIT'
+  CHAT: 'CHAT',
+  EXIT: 'EXIT',
 };
 
 function startup () {
@@ -24,12 +25,12 @@ function startup () {
 
   server = http.createServer(app);
   server.listen(port);
-  console.log("http server listening on %d", port);
+  console.log('http server listening on %d', port);
 
   wss = new WebSocketServer({server: server});
-  console.log("websocket server created");
+  console.log('websocket server created');
 
-  wss.on("connection", registerClient);
+  wss.on('connection', registerClient);
   setupTick();
 }
 
@@ -40,7 +41,7 @@ function setupTick () {
       clients: Object.keys(clients).length,
     };
     broadcast(Types.TICK, content);
-  }, 5000);
+  }, 1000);
 }
 
 function registerClient (ws) {
@@ -52,12 +53,45 @@ function registerClient (ws) {
     id: id,
     ws: ws,
   };
-  ws.on("close", () => unregisterClient(id));
+  ws.on('close', () => unregisterClient(id));
+  ws.on('message', (event, flags) => handleIncomingMessage(id, event, flags));
+  ws.on('error', (e) => handleError(id, e));
 }
 function unregisterClient (id) {
   console.log(`WebSocket ${id} closed connection`);
   delete clients[id];
   broadcast(Types.EXIT, {id: id});
+}
+function handleError (id, e) {
+  console.error(`Got error from ${id} with error ${e.message}`);
+  console.error(e);
+}
+function handleIncomingMessage (id, event, flags) {
+  if (flags.binary) {
+    return console.error('Backend received binary data but cant handle it');
+  }
+
+  var data = JSON.parse(event);
+  var content = data.content;
+
+  switch (data.type) {
+    case Types.CHAT: handleChat(id, content); break;
+    default: console.error('Can\'t handle ' + data.type);
+  }
+}
+function handleChat (id, content) {
+  if (content.id !== id) {
+    console.log('Bad impersonation');
+    return send(clients[id].ws, Types.CHAT, {
+      id: 'god',
+      message: 'Stop trying to impersonate user ' + content.id,
+      messageId: 1,
+    });
+  }
+  var msgId = content.messageId;
+  var msg = content.message;
+  console.log(`Message from ${id} (msgId ${msgId}): ${msg}`);
+  broadcast(Types.CHAT, content);
 }
 
 function broadcast(type, content) {
@@ -71,3 +105,9 @@ function send (ws, type, content) {
 }
 
 startup();
+
+setTimeout(() => broadcast(Types.CHAT, {
+  id: 'god',
+  messageId: 1,
+  message: 'You all work for me now!'
+}), 17000);
